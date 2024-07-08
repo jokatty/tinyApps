2b3c09883a19d1aba7c73383ffd0b7c838ed6dab
jokatty <jyotikattani@gmail.com>
2024-07-08 14:27:39 +0200

    add first ts app

---
+++ typescript/firstapp/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 25;
+if (age > 20) {
+    console.log('age is greater than 20');
+}
+//# sourceMappingURL=index.js.map
---
+++ typescript/firstapp/src/index.ts
@@ -0,0 +1,4 @@
+let age: number = 25;
+if (age > 20) {
+  console.log('age is greater than 20');
+}
