00a0e84cf6ae498e805b69f8bacba7fd93bcd834
jokatty <jyotikattani@gmail.com>
2024-07-15 11:39:52 +0200

    add css modult style

---
+++ nextjs/next-app/app/components/AddUser/AddUserButton.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import style from './AddUserButton.module.css';
+
+export default function AddUserButton() {
+  return <button className={style.buttonStyle}>add user</button>;
+}
--- nextjs/next-app/app/page.tsx
+++ nextjs/next-app/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import UsersPage from './users/page';
-import AddUser from './components/AddUser';
+import AddUser from './components/AddUser/AddUser';
 
 export default function Home() {
   return (
     <main>
       <h1>Hello, World!</h1>
-      <Link href='users/newuser'> new users</Link>
-      <AddUser />
+      <Link href='users/newuser'>
+        {' '}
+        <AddUser />
+      </Link>
     </main>
   );
 }
